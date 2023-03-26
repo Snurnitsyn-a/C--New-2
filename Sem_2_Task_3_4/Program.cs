34e4a78bb6b96621bbdd176bfb39d610a6da84f7
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-03-26 12:07:07 +0300

    solved 3rd and 4th task of the second seminar

---
+++ Sem_2_Task_3_4/Program.cs
@@ -0,0 +1,32 @@
+﻿/*int num1 = int.Parse(Console.ReadLine());
+
+if (num1 % 7 == 0 & num1 % 23 == 0)
+{
+    Console.WriteLine("The number is multiple to both 7 and 23");
+}
+else
+{
+    Console.WriteLine("The number isn't multiple to either 7 or 23");
+}*/
+
+
+/*Задача No16. Работа в группах
+Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.Write("Please input the first number: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Please input the second number: ");
+int num2 = int.Parse(Console.ReadLine());
+
+if (num2 * num2 == num1)
+{
+    Console.WriteLine("num1 is num2 squared");
+}
+else if (num1 * num1 == num2)
+{
+    Console.WriteLine("num2 is num1 squared");
+}
+else
+{
+    Console.WriteLine("none of the numbers are squares of each other");
+}    
+*/
