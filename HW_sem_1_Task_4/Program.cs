60f0013d01801d26c93389b7d355c3ff1220c432
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-03-30 00:56:53 +0300

    Seminar 1, task 4 solved

---
+++ HW_sem_1_Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+(int a, int b, int c) = (int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+int Max = a;
+if (b > a & b > c)
+{
+    Max = b;
+}
+if (c > a & c > a)
+{
+    Max = c;
+}
+Console.Write("max = ");
+Console.Write(Max);
