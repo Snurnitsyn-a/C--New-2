643190d940978f6ac8e923b07fac11233e2c3205
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-03-31 23:14:21 +0300

    Sem 2, Task 10 solved

--- HW_sem_2_Task_10/Program.cs
+++ HW_sem_2_Task_10/Program.cs
@@ -5,8 +5,15 @@
 782 -> 8
 918 -> 1*/
 
-
+Console.WriteLine("Enter a 3 digit number ");
 int N = int.Parse(Console.ReadLine());
 int mod_100 = N % 100;
 int result = mod_100 / 10;
-Console.WriteLine(result);
+if (N < 100 | N > 999)
+{
+    Console.WriteLine("The number has either more or less than 3 digits");    
+}
+else
+{
+    Console.WriteLine("The 2nd digit is " + result);
+}
