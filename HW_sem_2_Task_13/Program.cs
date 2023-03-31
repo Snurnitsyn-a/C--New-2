bc6931c009aa03858d08463851c411e14104a4a4
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-03-31 22:45:56 +0300

    Seminar 2, Task 13 solved

--- HW_sem_2_Task_13/Program.cs
+++ HW_sem_2_Task_13/Program.cs
@@ -7,3 +7,21 @@
 
 32679 -> 6*/
 
+int N = int.Parse(Console.ReadLine());
+int x = 1;
+int N1 = N / x;
+while (N1 > 999)
+{
+    x *= 10;
+    N1 = N / x;
+}
+int N2 = N /x;
+int result = N2 % 10;
+if (N2 < 100)
+{
+    Console.WriteLine("There is no third digit");    
+}
+else
+{
+    Console.WriteLine(result);
+}
